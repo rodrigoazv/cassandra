5837e20b3ac224b70e42790bb2e33aa84f6b38e9
Rodrigo Azevedo <rodrigo.azevedo@movile.com>
2022-07-28 23:02:10 -0300

    feat: add graphql and rest services

---
+++ ship/index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const listOfShips = require('./ship-list.json')
+
+app.get('/', (req, res) => {
+  res.send(listOfShips)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
